39eeb8bbdfc947dc15162f6693de786f40bfce27
Laiba Kamal <laibakamal003@gmail.com>
2022-09-12 20:24:10 +0500

    spalsh screen added

---
+++ QuranAppv2-master/app/src/main/java/com/example/quranappv2/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.quranappv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.Ac);
+    }
+
+}
