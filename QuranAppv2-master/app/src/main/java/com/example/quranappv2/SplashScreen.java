cf38a38da47d44907b2365026c1937939674ed63
Laiba Kamal <laibakamal003@gmail.com>
2022-09-13 01:28:43 +0500

    splash screen

--- QuranAppv2-master/app/src/main/java/com/example/quranappv2/SplashScreen.java
+++ QuranAppv2-master/app/src/main/java/com/example/quranappv2/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.quranappv2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +10,17 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
-        setContentView(R.layout.Ac);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, MainActivity.class);
+                intent.putExtra("qariIndex", "0");
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 
 }
